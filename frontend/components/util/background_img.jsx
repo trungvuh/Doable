ec42d5a165125d7bc12746fe02e612dc44a2c350
TV <trungvuh@gmail.com>
2017-11-30 23:23:59 -0800

    Finish splash background

--- frontend/components/util/background_img.jsx
+++ frontend/components/util/background_img.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 
-const imageArray = [
-  'http://res.cloudinary.com/trungvuh/image/upload/v1511815992/star-baby-girl-fantasy-ppt-backgrounds-powerpoint_mwebzy.jpg',
-  'http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1300/v1511340654/24_izm9b5.jpg',
-  'http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1511374128/25_bkagbn.jpg'];
-
 class BackgroundImg extends React.Component {
 
   render() {
     return (
       <div className="background" id="cf4a">
         <img
-          src='http://res.cloudinary.com/trungvuh/image/upload/v1511815992/star-baby-girl-fantasy-ppt-backgrounds-powerpoint_mwebzy.jpg'/>
-        <img
-          src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1300/v1511340654/24_izm9b5.jpg'/>
+          src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512110185/maxresdefault_daw1gt.jpg'/>
         <img
           src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1511374128/25_bkagbn.jpg'/>
         <img
-          src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1300/v1511340654/24_izm9b5.jpg'/>
+          src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512110268/maxresdefault_1_aryhrr.jpg'/>
+        <img
+          src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512112324/IMG_7301_xetlfv.jpg'/>
       </div>
     );
 
@@ -25,3 +20,23 @@ class BackgroundImg extends React.Component {
 }
 
 export default BackgroundImg;
+
+///Baby Picture
+//1 http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512110185/maxresdefault_daw1gt.jpg
+
+//2 http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512110268/maxresdefault_1_aryhrr.jpg
+
+//3 http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1511374128/25_bkagbn.jpg
+
+//4 http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1512112324/IMG_7301_xetlfv.jpg
+
+
+//old pic
+// <img
+//   src='http://res.cloudinary.com/trungvuh/image/upload/v1511815992/star-baby-girl-fantasy-ppt-backgrounds-powerpoint_mwebzy.jpg'/>
+// <img
+//   src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1300/v1511340654/24_izm9b5.jpg'/>
+// <img
+//   src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1500/v1511374128/25_bkagbn.jpg'/>
+// <img
+//   src='http://res.cloudinary.com/trungvuh/image/upload/c_fill,h_700,w_1300/v1511340654/24_izm9b5.jpg'/>
