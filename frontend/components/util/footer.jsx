83bf214f49d353ae2177b405d2587fb368b05dba
TV <trungvuh@gmail.com>
2017-12-09 00:24:51 -0800

    Fix link LinkedIn

--- frontend/components/util/footer.jsx
+++ frontend/components/util/footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => (
         src="http://res.cloudinary.com/trungvuh/image/upload/c_scale,h_40,w_40/v1511385079/github_momj6r.png"></img>
     </a>
 
-    <a href="https://www.linkedin.com/in/trung-vu-299584124/" target="_blank" className="linkedin">
+    <a href="https://www.linkedin.com/in/trungvuh/" target="_blank" className="linkedin">
       <img
         src="http://res.cloudinary.com/trungvuh/image/upload/c_scale,w_40/v1511386212/linkedin-black_ebf2z4.png" />
     </a>
