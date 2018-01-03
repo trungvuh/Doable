78d5a04acf527f2972a2f8b20e5874aeab1c399f
TV <trungvuh@gmail.com>
2018-01-03 10:27:53 -0800

    Adding link for homepage

--- frontend/components/util/footer.jsx
+++ frontend/components/util/footer.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 
 const Footer = () => (
   <div className='footer-links'>
-    <a
-      href="https://github.com/trungvuh"
-      target="_blank"
-      className="github">
+    <a href="https://github.com/trungvuh" target="_blank" className="github">
       <img
-        src="https://res.cloudinary.com/trungvuh/image/upload/c_scale,h_40,w_40/v1511385079/github_momj6r.png"></img>
+        src="https://res.cloudinary.com/trungvuh/image/upload/c_scale,h_40,w_40/v1511385079/github_momj6r.png">
+      </img>
     </a>
 
     <a href="https://www.linkedin.com/in/trungvuh/" target="_blank" className="linkedin">
       <img
-        src="https://res.cloudinary.com/trungvuh/image/upload/c_scale,w_40/v1511386212/linkedin-black_ebf2z4.png" />
+        src="https://res.cloudinary.com/trungvuh/image/upload/c_scale,w_40/v1511386212/linkedin-black_ebf2z4.png">
+      </img>
+    </a>
+
+    <a href="https://trungvuh.github.io/TV/" target="_blank" className="linkedin">
+      <img
+        src="https://res.cloudinary.com/trungvuh/image/upload/v1515003657/Homepage_ujexda.png">
+      </img>
     </a>
 
   </div>
